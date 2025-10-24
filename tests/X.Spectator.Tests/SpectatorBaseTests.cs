fae4690c0980a40fedbb944fe192005bba6afd82
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-10-25 01:14:02 +0300

    Add unit tests for Probe and SpectatorBase classes to ensure correct behavior

---
+++ tests/X.Spectator.Tests/SpectatorBaseTests.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+using X.Spectator.Base;
+using X.Spectator.Spectators;
+using Xunit;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace X.Spectator.Tests
+{
+    public class SpectatorBaseTests
+    {
+        [Fact]
+        public void SpectatorBase_StateTransitions()
+        {
+            var probe1States = new Queue<ProbeResult>(
+                new[]
+                {
+                    Create(true),
+                    Create(true),
+                    Create(true),
+                    Create(false),
+                    Create(true),
+                    Create(true),
+                    Create(true),
+                    Create(true)
+                });
+
+            var probe2States = new Queue<ProbeResult>(
+                new[]
+                {
+                    Create(true),
+                    Create(true),
+                    Create(false),
+                    Create(true),
+                    Create(false),
+                    Create(true),
+                    Create(true),
+                    Create(true)
+                });
+
+            IProbe probe1 = new Probe("Test-1", () =>
+            {
+                var result = probe1States.Dequeue();
+                return Task.FromResult(result);
+            });
+
+            IProbe probe2 = new Probe("Test-2", () =>
+            {
+                var result = probe2States.Dequeue();
+                return Task.FromResult(result);
+            });
+
+            var stateEvaluatorMock = new Mock<IStateEvaluator<HealthStatus>>();
+
+            stateEvaluatorMock
+                .Setup(o => o.Evaluate(
+                    It.IsAny<HealthStatus>(),
+                    It.IsAny<DateTime>(),
+                    It.IsAny<IReadOnlyCollection<JournalRecord>>()))
+                .Returns((HealthStatus currentState,
+                    DateTime stateChangedLastTime,
+                    IReadOnlyCollection<JournalRecord> journal) =>
+                {
+                    var data = journal.TakeLast(3).ToImmutableList();
+
+                    var totalChecks = data.Count;
+                    var failedChecks = data.Count(o => o.Values.Any(v => v.Value.Status == HealthStatus.Unhealthy));
+
+                    if (failedChecks == 0)
+                    {
+                        return HealthStatus.Healthy;
+                    }
+
+                    if (failedChecks == 1)
+                    {
+                        return HealthStatus.Degraded;
+                    }
+
+                    return HealthStatus.Unhealthy;
+                });
+
+            var stateEvaluator = stateEvaluatorMock.Object;
+            var retentionPeriod = TimeSpan.FromMinutes(10);
+
+            var spectator = new SpectatorBase<HealthStatus>(stateEvaluator, retentionPeriod, HealthStatus.Unhealthy);
+
+            spectator.AddProbe(probe1);
+            spectator.AddProbe(probe2);
+
+            var states = new List<HealthStatus>();
+
+            spectator.HealthChecked += (sender, args) => { };
+
+            spectator.StateChanged += (sender, args) => { states.Add(args.State); };
+
+            for (int i = 0; i < 8; i++)
+            {
+                spectator.CheckHealth();
+            }
+
+            var expected = new[]
+            {
+                HealthStatus.Healthy,
+                HealthStatus.Degraded,
+                HealthStatus.Unhealthy,
+                HealthStatus.Degraded,
+                HealthStatus.Healthy
+            };
+
+            Assert.Equal(expected.ToArray(), states.ToArray());
+        }
+
+        private static ProbeResult Create(bool value) =>
+            new()
+            {
+                Value = value ? HealthCheckResult.Healthy() : HealthCheckResult.Unhealthy(),
+                Time = DateTime.UtcNow,
+                ProbeName = "TEST"
+            };
+    }
+}
